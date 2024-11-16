3c80224be9d7b4f888b09c25081aa637b5e66b82
Jibran Jan <jibranjan77@gmail.com>
2024-11-16 16:52:05 +0530

    add conditions to check correct or wrong number

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
 'use strict';
 
-document.addEventListener('DOMContentLoaded', function() {
-    document.querySelector('.check').addEventListener(`click`, function() {
-        const guess = Number(document.querySelector('.guess').value);
-        if (!guess) {
-            document.querySelector('.message').textContent = '⛔ No number!';
-        }
-    });
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+const $secretNumber = document.querySelector('.number');
+
+
+document.querySelector('.check').addEventListener(`click`, function() {
+    const guess = Number(document.querySelector('.guess').value);
+    const message = document.querySelector('.message');
+
+    if (!guess) {
+        message.textContent = '⛔ No number!';
+    } else if (guess === secretNumber) {
+        message.textContent = '🎉 Correct Number!';
+        $secretNumber.textContent = secretNumber;
+    } else if (guess > (secretNumber + secretNumber/2)) {
+        message.textContent = '📈 Too high!';
+    } else if (guess > secretNumber) {
+        message.textContent = '📈 High!';
+    } else if (guess < (secretNumber - secretNumber/2)) {
+        message.textContent = '📉 Too low!';
+    } else if (guess < secretNumber) {
+        message.textContent = '📉 Low!';
+    }
 });
